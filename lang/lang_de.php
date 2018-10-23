10f5208adeb2e85737429b5659d97bf46067a3a2
djphil <djphil@users.noreply.github.com>
2018-10-24 01:22:13 +0200

    Add lang_de.php
    Add lang_de.php

---
+++ lang/lang_de.php
@@ -0,0 +1,19 @@
+<?php if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {die('Access denied ...');} ?>
+<?php
+$language_code='de';
+$actual_language='German';
+
+/* Buttons */
+$txt_north = "Nord";
+$txt_south = "Süd";
+$txt_west = "West";
+$txt_east = "Ost";
+$txt_center = "Center";
+$txt_search = "Suche";
+
+/* Legend */
+$txt_free = "Verfügbar";
+$txt_main = "Center";
+$txt_single = "Besetzt (Einzige region)";
+$txt_var = "Besetzt (Variable region)";
+?>
